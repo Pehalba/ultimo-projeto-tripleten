4a335118f537f69804981f31b690e6e714d5007e
Pedro Alba <pedroalbaadm@gmail.com>
2026-04-28 13:07:56 -0300

    style: adiciona fontes woff reais e atualiza font-face

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import './Header.css'
+import { Link } from 'react-router-dom'
 import Navigation from '../Navigation/Navigation'
 
 function Header() {
   return (
     <header className="header">
-      <h1 className="header__title">Projeto Final TripleTen</h1>
+      <h1 className="header__title">
+        <Link className="header__home-link" to="/">
+          Projeto Final TripleTen
+        </Link>
+      </h1>
       <Navigation />
     </header>
   )
